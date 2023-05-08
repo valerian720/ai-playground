7fd1f895f25b96df63eec1cdc114d8a2f78003ae
Валериан Воротников <valerian7202@gmail.com>
2023-05-08 14:22:30 +0300

    script

---
+++ eslintAutoFix.bat
@@ -0,0 +1,12 @@
+@REM add to vue.config.js
+@REM const { defineConfig } = require("@vue/cli-service");
+@REM module.exports = defineConfig({
+@REM   transpileDependencies: true,
+@REM   // auto fix on save
+@REM   chainWebpack: (config) => {
+@REM     config.plugin("eslint").tap((args) => {
+@REM       args[0].fix = true;
+@REM       return args;
+@REM     });
+@REM   },
+@REM });
